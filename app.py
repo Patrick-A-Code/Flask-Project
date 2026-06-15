12dd2a604a7b66ffdc9638ffac567f5c902ec238
S2500359 <s2500359@ndonline.ac.uk>
2026-06-15 10:53:32 +0100

    Started to populate HTML pages

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home_page():  # put application's code here
+    return render_template("home.html")
 
+@app.route('/login')
+def login_page():  # put application's code here
+    return render_template("login.html")
+
+@app.route('/register')
+def register_page():  # put application's code here
+    return render_template("register.html")
 
 if __name__ == '__main__':
     app.run()
